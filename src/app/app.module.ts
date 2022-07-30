2d27bdeb37b7d1f5573a88ff67eb45a5f90ea2ec
Adrian Termenon <adrianriera@gmail.com>
2022-07-31 01:15:22 +0200

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
-import { AnimationsService } from './services/animations.service';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
-
+import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
 import { FichaComponent } from './components/ficha/ficha.component';
-import { TableroComponent } from './components/tablero/tablero.component';
-import { JuegoService } from './services/juego.service';
+import { GameInfoComponent } from './components/game-info/game-info.component';
+import { EndGameFormComponent } from './components/gui/end-game-form/end-game-form.component';
 import { GuiComponent } from './components/gui/gui.component';
+import { GameListComponent } from './components/gui/new-game-form/game-list/game-list.component';
+import { NewGameFormComponent } from './components/gui/new-game-form/new-game-form.component';
+import { NewUserFormComponent } from './components/gui/new-user-form/new-user-form.component';
+import { WaitingDialogComponent } from './components/gui/waiting-dialog/waiting-dialog.component';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { HammertimeDirective } from './directives/hammertime.directive';
+import { AnimationsService } from './services/animations.service';
+import { JuegoService } from './services/juego.service';
 import { SocketsService } from './services/sockets.service';
 
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { NewUserFormComponent } from './components/gui/new-user-form/new-user-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NewGameFormComponent } from './components/gui/new-game-form/new-game-form.component';
-import { GameListComponent } from './components/gui/new-game-form/game-list/game-list.component';
-import { EndGameFormComponent } from './components/gui/end-game-form/end-game-form.component';
-import { GameInfoComponent } from './components/game-info/game-info.component';
-
-const config: SocketIoConfig = { url: 'ws://localhost:8080', options: {'forceNew': true}};
-
+const config: SocketIoConfig = {
+  url: environment.server,
+  options: { reconnection: true },
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,14 +35,17 @@ const config: SocketIoConfig = { url: 'ws://localhost:8080', options: {'forceNew
     GameListComponent,
     EndGameFormComponent,
     GameInfoComponent,
+    HammertimeDirective,
+    WaitingDialogComponent,
+    ChatComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
   ],
   providers: [JuegoService, AnimationsService, SocketsService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
